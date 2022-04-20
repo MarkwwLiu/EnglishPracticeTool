fa76248590857c319189b72928c6cda4529f4a90
Mark <ak790718@gmail.com>
2022-04-20 14:12:02 +0800

    update main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+class EnglishPracticeTool:
+    
+    def __init__(self):
+        pass
+    
+    def main(self):
+        print("test")
+        
+if __name__ == '__main__':
+    
+    run = EnglishPracticeTool()
+    run.main()
