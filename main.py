14f484ba7ffb3313a6da34161c0a3a35cc2d9b41
Mark <ak790718@gmail.com>
2022-04-20 18:28:30 +0800

    新增功能 移除不必要特殊符號 字串轉換為List

--- main.py
+++ main.py
@@ -2,10 +2,23 @@
 class EnglishPracticeTool:
     
     def __init__(self):
-        pass
-    
+        # self.input_english_sentence = input("輸入要練習的英文句子: ")
+        self.input_english_sentence = "how are you?"
+        self.special_symbol_removal_list = [',', '?', '.']
+        self.hide_word_number = 1
+        
     def main(self):
-        print("test")
+        # 先移除特殊符號
+        for remove_special_symbol in self.special_symbol_removal_list:
+            self.input_english_sentence = self.input_english_sentence.replace(remove_special_symbol, '')
+        # 轉換為 list 
+        input_english_sentence_to_list = self.input_english_sentence.split(' ')
+        
+        print(input_english_sentence_to_list)
+        
+        
+        
+        
         
 if __name__ == '__main__':
     
